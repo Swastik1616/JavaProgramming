da7edaf1313050506a6ff1c110f8ed234341d4b4
Swastik555 <swastikgondhi2204@gmail.com>
2025-03-04 21:25:12 +0530

    Part-1-Q9
    Printing the standard Star Pyramid

--- Part 1/StarPyramid.java
+++ Part 1/StarPyramid.java
@@ -5,6 +5,7 @@ public class StarPyramid {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int rows = sc.nextInt();
+        
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= rows-i; j++) { 
                 System.out.print(" ");
