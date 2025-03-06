652e0225550dbc2c2ab54c50e970e68e78eb2634
Swastik555 <swastikgondhi2204@gmail.com>
2025-03-06 12:14:57 +0530

    Create ExceptionHandling.java

---
+++ ClassWork/ExceptionHandling.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HandleException{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n1,n2;
+        try{
+        System.out.println("START.....");
+        System.out.print("Enter the first number: ");
+        n1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        n2 = sc.nextInt();
+        //System.out.println();
+        int result = n1/n2;2
+        System.out.println("The quotient is: "+ result);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Second number should not be zero");
+            System.out.println(e.getMessage());
+        }
+    }
+}
